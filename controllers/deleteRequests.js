03bb81507cf266008a57332ab5f5c5883a14d0d2
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2017-07-27 08:48:42 +0600

    delete requests

--- controllers/deleteRequests.js
+++ controllers/deleteRequests.js
@@ -1,218 +1,272 @@
-var Promise = require('bluebird')
 var jwt = require('jsonwebtoken')
-var pool = require('../connection/pool')
+var async = require('async')
+var connection = require('../connection/pool')
 var ensureToken = require('./tokens')
 
-module.exports = function(app)
-{
-  app.delete('/tables/:id', function(request, response)
-  {
-    var query = "DELETE FROM tables WHERE id = " + request.params.id
-    Promise.using(pool(), function(connection)
-    {
-      return connection.query(query)
-      .then(function(result)
-      {
-        if(result.affectedRows > 0)
-        {
-          response.send({error: ""})
-        }
-        else
-        {
-          response.status(500).send({error: "there is no such a table"})
-        }
-      })
-      .catch(function(error)
-      {
-        response.status(404).send({error: error})
-      })
+module.exports = function(app) {
+  app.delete('/tables/:id', ensureToken, function(request, response) {
+    jwt.verify(request.token, request.headers['login'], function(error, data) {
+      if(error) {
+        response.status(404).send({error: "invalid heasder"})
+      }
+      else {
+        var query = "DELETE FROM tables WHERE id = " + request.params.id
+        connection.query(query, function(error, result) {
+          if(error) {
+            response.status(500).send({error: "some internal error"})
+          }
+          else {
+            if(result.affectedRows > 0) {
+              response.send({error: ""})
+            }
+            else {
+              response.status(400).send({error: "there is no such a table"})
+            }
+          }
+        })
+      }
     })
   })
 
-  app.delete('/roles/:id', function(request, response)
-  {
-    var query = "DELETE FROM roles WHERE id = " + request.params.id
-    Promise.using(pool(), function(connection)
-    {
-      return connection.query(query)
-      .then(function(result)
-      {
-        if(result.affectedRows > 0)
-        {
-          response.send({error: ""})
-        }
-        else
-        {
-          response.status(500).send({error: "there is no such a role"})
-        }
-      })
-      .catch(function(error)
-      {
-        response.status(404).send({error: error})
-      })
+  app.delete('/roles/:id', ensureToken, function(request, response) {
+    jwt.verify(request.token, request.headers['login'], function(error, data) {
+      if(error) {
+        response.status(404).send({error: "invalid heasder"})
+      }
+      else {
+        var query = "DELETE FROM roles WHERE id = " + request.params.id
+        connection.query(query, function(error, result) {
+          if(error) {
+            response.status(500).send({error: "some internal error"})
+          }
+          else {
+            if(result.affectedRows > 0) {
+              response.send({error: ""})
+            }
+            else {
+              response.status(500).send({error: "there is no such a role"})
+            }
+          }
+        })
+      }
     })
   })
 
-  app.delete('/departments/:id', function(request, response) {
-    var query = "DELETE FROM departments WHERE id = " + request.params.id
-    Promise.using(pool(), function(connection) {
-      return connection.query(query)
-      .then(function(result) {
-        if(result.affectedRows > 0) {
-          response.send({error: ""})
-        }
-        else {
-          response.status(500).send({error: "there is no such a department"})
-        }
-      })
-      .catch(function(error) {
-        response.status(404).send({error: error})
-      })
+  app.delete('/departments/:id', ensureToken, function(request, response) {
+    jwt.verify(request.token, request.headers['login'], function(error, data) {
+      if(error) {
+        response.status(404).send({error: "invalid heasder"})
+      }
+      else {
+        var query = "DELETE FROM departments WHERE id = " + request.params.id
+        connection.query(query, function(error, result) {
+          if(error) {
+            response.status(500).send({error: "some internal error"})
+          }
+          else {
+            if(result.affectedRows > 0) {
+              response.send({error: ""})
+            }
+            else {
+              response.status(500).send({error: "there is no such a department"})
+            }
+          }
+        })
+      }
     })
   })
 
-  app.delete('/users/:id', function(request, response) {
-    var query = "DELETE FROM users WHERE id = " + request.params.id
-    Promise.using(pool(), function(connection) {
-      return connection.query(query)
-      .then(function(result) {
-        if(result.affectedRows > 0) {
-          response.send({error: ""})
-        }
-        else {
-          response.status(500).send({error: "there is no such an user"})
-        }
-      })
-      .catch(function(error) {
-        response.status(404).send({error: error})
-      })
+  app.delete('/users/:id', ensureToken, function(request, response) {
+    jwt.verify(request.token, request.headers['login'], function(error, data) {
+      if(error) {
+        response.status(404).send({error: "invalid heasder"})
+      }
+      else {
+        var query = "DELETE FROM users WHERE id = " + request.params.id
+        connection.query(query, function(error, result) {
+          if(error) {
+            response.status(500).send({error: "some internal error"})
+          }
+          else {
+            if(result.affectedRows > 0) {
+              response.send({error: ""})
+            }
+            else {
+              response.status(500).send({error: "there is no such an user"})
+            }
+          }
+        })
+      }
     })
   })
 
-  app.delete('/mealCategories/:id', function(request, response) {
-    var query = "DELETE FROM categories WHERE id = " + request.params.id
-    Promise.using(pool(), function(connection) {
-      return connection.query(query)
-      .then(function(result) {
-        if(result.affectedRows > 0) {
-          response.send({error: ""})
-        }
-        else {
-          response.status(500).send({error: "there is no such a category"})
-        }
-      })
-      .catch(function(error) {
-        response.status(404).send({error: error})
-      })
+  app.delete('/mealCategories/:id', ensureToken, function(request, response) {
+    jwt.verify(request.token, request.headers['login'], function(error, data) {
+      if(error) {
+        response.status(404).send({error: "invalid heasder"})
+      }
+      else {
+        var query = "DELETE FROM categories WHERE id = " + request.params.id
+        connection.query(query, function(error, result) {
+          if(error) {
+            response.status(500).send({error: "some internal error"})
+          }
+          else {
+            if(result.affectedRows > 0) {
+              response.send({error: ""})
+            }
+            else {
+              response.status(500).send({error: "there is no such a category"})
+            }
+          }
+        })
+      }
     })
   })
 
-  app.delete('/statuses/:id', function(request, response) {
-    var query = "DELETE FROM statuses WHERE id = " + request.params.id
-    Promise.using(pool(), function(connection) {
-      return connection.query(query)
-      .then(function(result) {
-        if(result.affectedRows > 0) {
-          response.send({error: ""})
-        }
-        else {
-          response.status(500).send({error: "there is no such a status"})
-        }
-      })
-      .catch(function(error) {
-        response.status(404).send({error: error})
-      })
+  app.delete('/statuses/:id', ensureToken, function(request, response) {
+    jwt.verify(request.token, request.headers['login'], function(error, data) {
+      if(error) {
+        response.status(404).send({error: "invalid heasder"})
+      }
+      else {
+        var query = "DELETE FROM statuses WHERE id = " + request.params.id
+        connection.query(query, function(error, result) {
+          if(error) {
+            response.status(500).send({error: "some internal error"})
+          }
+          else {
+            if(result.affectedRows > 0) {
+              response.send({error: ""})
+            }
+            else {
+              response.status(500).send({error: "there is no such a status"})
+            }
+          }
+        })
+      }
     })
   })
 
-  app.delete('/servicePercentage', function(request, response) {
-    var query = "DELETE FROM servicepercentage"
-    Promise.using(pool(), function(connection) {
-      return connection.query(query)
-      .then(function(result) {
-        if(result.affectedRows > 0) {
-          response.send({error: ""})
-        }
-        else {
-          response.status(500).send({error: "there is nothing to delete"})
-        }
-      })
-      .catch(function(error) {
-        response.status(404).send({error: error})
-      })
+  app.delete('/servicePercentage', ensureToken, function(request, response) {
+    jwt.verify(request.token, request.headers['login'], function(error, data) {
+      if(error) {
+        response.status(404).send({error: "invalid heasder"})
+      }
+      else {
+        var query = "DELETE FROM variables WHERE name = 'percentage' "
+        connection.query(query, function(error, result) {
+          if(error) {
+            response.status(500).send({error: "some internal error"})
+          }
+          else {
+            if(result.affectedRows > 0) {
+              response.send({error: ""})
+            }
+            else {
+              response.status(500).send({error: "there is nothing to delete"})
+            }
+          }
+        })
+      }
     })
   })
 
-  app.delete('/meals/:id', function(request, response) {
-    var query = "DELETE FROM meals WHERE id = " + request.params.id
-    Promise.using(pool(), function(connection) {
-      return connection.query(query)
-      .then(function(result) {
-        if(result.affectedRows > 0) {
-          response.send({error: ""})
-        }
-        else {
-          response.status(500).send({error: "there is no such a meal"})
-        }
-      })
-      .catch(function(error) {
-        response.status(404).send({error: error})
-      })
+  app.delete('/meals/:id', ensureToken, function(request, response) {
+    jwt.verify(request.token, request.headers['login'], function(error, data) {
+      if(error) {
+        response.status(404).send({error: "invalid heasder"})
+      }
+      else {
+        var query = "DELETE FROM meals WHERE id = " + request.params.id
+        connection.query(query, function(error, result) {
+          if(error) {
+            response.status(500).send({error: "some internal error"})
+          }
+          else {
+            if(result.affectedRows > 0) {
+              response.send({error: ""})
+            }
+            else {
+              response.status(500).send({error: "there is no such a meal"})
+            }
+          }
+        })
+      }
     })
   })
 
-  app.delete('/orders/:id', function(request, response) {
-    var query = "DELETE FROM orders WHERE id = " + request.params.id
-    Promise.using(pool(), function(connection) {
-      return connection.query(query)
-      .then(function(result) {
-        if(result.affectedRows > 0) {
-          response.send({error: ""})
-        }
-        else {
-          response.status(500).send({error: "there is no such an order"})
-        }
-      })
-      .catch(function(error) {
-        response.status(404).send({error: error})
-      })
+  // app.delete('/orders/:id', ensureToken, function(request, response) {
+  //   jwt.verify(request.token, request.headers['login'], function(error, data) {
+  //     if(error) {
+  //       response.status(404).send({error: "invalid heasder"})
+  //     }
+  //     else {
+  //       var query = "DELETE FROM orders WHERE id = " + request.params.id
+  //       connection.query(query, function(error, result) {
+  //         if(error) {
+  //           response.status(500).send({error: "some internal error"})
+  //         }
+  //         else {
+  //           if(result.affectedRows > 0) {
+  //             response.send({error: ""})
+  //           }
+  //           else {
+  //             response.status(500).send({error: "there is no such an order"})
+  //           }
+  //         }
+  //       })
+  //     }
+  //   })
+  // })
+
+  app.delete('/checks/:id', ensureToken, function(request, response) {
+    jwt.verify(request.token, request.headers['login'], function(error, data) {
+      if(error) {
+        response.status(404).send({error: "invalid heasder"})
+      }
+      else {
+        var query = "DELETE FROM checks WHERE id = " + request.params.id
+        connection.query(query, function(error, result) {
+          if(error) {
+            response.status(500).send({error: "some internal error"})
+          }
+          else {
+            if(result.affectedRows > 0) {
+              response.send({error: ""})
+            }
+            else {
+              response.status(500).send({error: "there is no such a check"})
+            }
+          }
+        })
+      }
     })
   })
 
-  app.delete('/checks/:id', function(request, response) {
-    var query = "DELETE FROM checks WHERE id = " + request.params.id
-    Promise.using(pool(), function(connection) {
-      return connection.query(query)
-      .then(function(result) {
-        if(result.affectedRows > 0) {
-          response.send({error: ""})
-        }
-        else {
-          response.status(500).send({error: "there is no such a check"})
-        }
-      })
-      .catch(function(error) {
-        response.status(404).send({error: error})
-      })
-    })
-  })
+  // app.delete('/mealsToOrder/:orderid/:mealid', function(request, response) {
+  //   jwt.verify(request.token, request.headers['login'], function(error, data) {
+  //     if(error) {
+  //       response.status(404).send({error: "invalid heasder"})
+  //     }
+  //     else {
+  //       var query = "DELETE FROM mealfororder WHERE orderid = " + request.params.orderid
+  //                 + " AND mealid = " + request.params.mealid
+  //       connection.query(query, function(error, result) {
+  //         if(error) {
+  //           response.status(500).send({error: "some internal error"})
+  //         }
+  //         else {
+  //           if(result.affectedRows > 0) {
+  //             response.send({error: ""})
+  //           }
+  //           else {
+  //             response.status(500).send({error: "there is no such a meal in this order"})
+  //           }
+  //         }
+  //       })
+  //     }
+  //   })
+  // })
 
-  app.delete('/mealsToOrder/:orderid/:mealid', function(request, response) {
-    var query = "DELETE FROM mealfororder WHERE orderid = " + request.params.orderid
-              + " AND mealid = " + request.params.mealid
-    Promise.using(pool(), function(connection) {
-      return connection.query(query)
-      .then(function(result) {
-        if(result.affectedRows > 0) {
-          response.send({error: ""})
-        }
-        else {
-          response.status(500).send({error: "there is no such a meal in this order"})
-        }
-      })
-      .catch(function(error) {
-        response.status(404).send({error: error})
-      })
-    })
-  })
 }
