6adb31ea84b276bd3fc5927f0c7a40dca597602f
= <=>
2017-08-03 16:05:28 +0600

    update on put requestss

--- connection/pool.js
+++ connection/pool.js
@@ -4,11 +4,7 @@ const pool = mysql.createConnection
 ({
     host: 'localhost',
     user: 'root',
-<<<<<<< HEAD
     password: 's0mer@nd0M',
-=======
-    password: 'root',
->>>>>>> 85369ab3c1e3e3bd0ac323477e9f0e0c4f0c728e
     database: 'crmfood',
     multipleStatements: true
 })
